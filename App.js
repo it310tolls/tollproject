7e68e7b2b9d2364d8b303d2758fb8962e23dfc41
BrandonPriestley <47763305+BrandonPriestley@users.noreply.github.com>
2019-02-28 19:26:07 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Container, Header, Content, Form, Item, Input, Label, Image, Button } from 'native-base';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  static navigationOptions =
+  {
+    title: "Home",
+
+    headerStyle:
+    {
+      backgroundColor: "blue",
+    },
+
+    headerTintColor: 'white',
+
+
+  };
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+          <Button onPress={() => this.props.navigation.navigate('Second')}>
+            <Text>Hit Me</Text>
+          </Button>
+        </Content>
+      </Container>
+
+
+    );
+  }
+}
+
+const styles = StyleSheet.create(
+{
+
+});
